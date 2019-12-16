42bc1d6787d09a2903ae83436ec752eb8be8a104
Novrii <38958308+Novrii@users.noreply.github.com>
2019-12-16 21:48:27 +0700

    Initial commit

---
+++ findfaces.py
@@ -0,0 +1,61 @@
+import face_recognition
+import os
+import cv2
+
+image = face_recognition.load_image_file('./img/groups/team2.jpg')
+face_locations = face_recognition.face_locations(image)
+
+# fungsi rescale image 
+def scale_down(scale, img):
+    width = int(img.shape[1] * scale/100)
+    height = int(img.shape[0] * scale/100)
+    dim = (width, height)
+    new_image = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    return new_image
+# fungsi cek_size
+def cek_size(size, img):
+    if size > 100000 and size < 200000:
+        scale_percent = 70
+        rescale = scale_down(scale_percent, img)
+        cv2.imwrite("test.jpg", rescale)
+        new_size = os.path.getsize("test.jpg")
+        print(f"New Size: {new_size/1024} (100)")
+    elif size > 200000 and size < 300000:
+        scale_percent = 60
+        rescale = scale_down(scale_percent, img)
+        cv2.imwrite("test.jpg", rescale)
+        new_size = os.path.getsize("test.jpg")
+        print(f"New Size: {new_size/1024} (200)")
+    elif size > 300000 and size < 400000:
+        scale_percent = 50
+        rescale = scale_down(scale_percent, img)
+        cv2.imwrite("test.jpg", rescale)
+        new_size = os.path.getsize("test.jpg")
+        print(f"New Size: {new_size/1024} (300)")
+    elif size > 400000 and size < 500000:
+        scale_percent = 40
+        rescale = scale_down(scale_percent, img)
+        cv2.imwrite("test.jpg", rescale)
+        new_size = os.path.getsize("test.jpg")
+        print(f"New Size: {new_size/1024} (400)")
+    elif size > 500000:
+        scale_percent = 30
+        rescale = scale_down(scale_percent, img)
+        cv2.imwrite("test.jpg", rescale)
+        new_size = os.path.getsize("test.jpg")
+        print(f"New Size: {new_size/1024} (500)")
+
+# Array of coords of each face
+# print(face_locations)
+
+print(f'There are {len(face_locations)} people in this image')
+
+path = './data_latih_kelas_e/MUHAMMAD HAFIZD ANSHARI/-Lvx1f7zABzldy7K4iqy-saya2.jpg'
+# testing size
+size = os.path.getsize(path)
+print(f"Size: {size/1024}")
+# resize
+gray_img = cv2.imread(path)
+
+cek_size(size, gray_img)
---
+++ testing_latih.py
@@ -0,0 +1,45 @@
+import face_recognition
+import pickle
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# gambar_dir = os.path.join(BASE_DIR, "data_latih_kelas_e")
+gambar_dir = os.path.join(BASE_DIR, "img\known")
+
+known_face_encodings = []
+known_face_names = []
+
+# print(gambar_dir)
+
+# for root, dirs, files in os.walk(gambar_dir):
+#     for file in files:
+#         print(os.path.join(root, file))
+
+for root, dirs, files in os.walk(gambar_dir):
+    for file in files:
+        if file.endswith("jpg"): # only jpg file
+            # print(os.path.join(root, file))
+            path = os.path.join(root, file)
+            base_name = os.path.basename(path)
+            name = base_name.strip('.jpg')
+            label = name.replace(" ", "")
+            # split base_name to make an array of string 
+            # use cv2 to resize image
+            
+            # print(label)
+            print(f"[INFO] Load {path}")
+            load_image = face_recognition.load_image_file(path)
+            face_encoding = face_recognition.face_encodings(load_image)[0]
+            known_face_encodings.append(face_encoding)
+            known_face_names.append(name)
+
+# print(known_face_encodings)
+# write pickle file face encodings and file name
+with open("face_encodings.pickle", "wb") as f:
+    pickle.dump(known_face_encodings, f)
+    print("[SUCCESS] Saved file face_encodings.pickle")
+
+with open("face_names.pickle", "wb") as name_file:
+    pickle.dump(known_face_names, name_file)
+    print("[SUCCESS] Saved file face_names.pickle")
+
