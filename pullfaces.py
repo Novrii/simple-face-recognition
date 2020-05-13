57878bb39279df2f1cc61c0ac7b4d7ad374c2cf5
Novrii <38958308+Novrii@users.noreply.github.com>
2020-05-14 05:58:38 +0700

    add template & config_fir

--- pullfaces.py
+++ pullfaces.py
@@ -1,13 +1,27 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import face_recognition
 
-image = face_recognition.load_image_file('./knn/IMG_CROP_E.jpg')
+image = face_recognition.load_image_file('test/4.jpg')
 face_locations = face_recognition.face_locations(image)
+
+# Convert to PIL format
+pil_image = Image.fromarray(image)
+
+# Create a ImageDraw instance
+draw = ImageDraw.Draw(pil_image)
+
+print(face_locations)
 
 for face_location in face_locations:
     top, right, bottom, left = face_location
 
-    face_image = image[top:bottom, left:right]
-    pil_image = Image.fromarray(face_image)
+    # face_image = image[top:bottom, left:right]
+    # pil_image = Image.fromarray(face_image)
     # pil_image.show()
-    pil_image.save(f'knn/{top}.jpg')
+    # Draw box
+    draw.rectangle(((left, top), (right, bottom)), outline=(255,255,0))
+    # pil_image.save(f'knn/{top}.jpg')
+
+del draw
+
+pil_image.show()
